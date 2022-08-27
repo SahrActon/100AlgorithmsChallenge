7b7b02984d001c0c2ef781786a6d4064d325c49e
Sahr Thomas Acton <tommy-acton@hotmail.co.uk>
2022-08-27 17:03:40 +0100

    Pushing my current changes

--- addBorder/addBorder.ts
+++ addBorder/addBorder.ts
@@ -1,5 +1,16 @@
-export function addBorder(picture: string[]): string[] {
+function addBorder(picture: string[]): any {
+    
+    const wall = '*'.repeat(picture[0].length + 2)
 
+    picture.unshift(wall); // this is will shot the wall to the front 
+    picture.push(wall) // this is will shot the wall to the back
+    
+
+    for (let i =1; i< picture.length -1; i++){
+        picture[i] = '*'.concat(picture[i], '*')
+    }
+    
+    return picture;
 }
 
-// console.log(addBorder(["abc", "ded"]));
+console.log(addBorder(["abc", "ded"]));
